7f0bbe0ac3343cef5363682a298d5d1aa47b4adf
yannick <yannickbaudraz@gmail.com>
2021-09-07 00:50:30 +0200

    Add layout
    Upgrade to bootstrap v5.0.2

--- views/home.php
+++ views/home.php
@@ -1,58 +1,54 @@
 <?php
 /** @var $trafficLight \TrafficLight\Models\TrafficLight */
+
 /** @var $this \TrafficLight\Controllers\TrafficLightController */
 
 use TrafficLight\Models\LampState;
 use TrafficLight\Models\LightState;
 
-$isOOS = $this->trafficLight->getLightState() == LightState::OOS;
+ob_start();
 
+$isOOS = $this->trafficLight->getLightState() == LightState::OOS;
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Traffic Light</title>
-    <link rel="stylesheet" href="/views/styles.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="d-flex justify-content-center mt-1">
-        <div class="d-flex flex-column">
-            <div class="light mb-1">
-                <span class="dot <?= $this->trafficLight->getRed() == LampState::ON ? 'bg-danger' : 'off' ?>"></span>
-                <?php
-                if ($isOOS):
-                    ?>
-                    <span class="dot l-yellow blink"></span>
-                <?php
-                else: ?>
-                    <span class="dot <?= $this->trafficLight->getYellow() == LampState::ON ? 'l-yellow' : 'off'
-                    ?>"></span>
-                <?php
-                endif ?>
-                <span class="dot <?= $this->trafficLight->getGreen() == LampState::ON ? 'bg-success' : 'off' ?>"></span>
-            </div>
-
-            <a
-              class="btn btn-primary align-self-center mb-1"
-              href="index.php?action=next&l-sequence=<?= $this->trafficLight->getLightState() ?>"
-            >
-                Suivant
-            </a>
-
+<div class="d-flex justify-content-center mt-1">
+    <div class="d-flex flex-column">
+        <div class="light mb-1">
+            <span class="dot <?= $this->trafficLight->getRed() == LampState::ON ? 'l-red' : 'off' ?>"></span>
             <?php
-            if (!$isOOS): ?>
-                <a
-                  class="btn btn-warning align-self-center"
-                  href="index.php?action=oos"
-                >
-                    Hors service
-                </a>
+            if ($isOOS):
+                ?>
+                <span class="dot l-yellow blink"></span>
+            <?php
+            else: ?>
+                <span class="dot <?= $this->trafficLight->getYellow() == LampState::ON ? 'l-yellow' : 'off'
+                ?>"></span>
             <?php
             endif ?>
+            <span class="dot <?= $this->trafficLight->getGreen() == LampState::ON ? 'l-green' : 'off' ?>"></span>
         </div>
+
+        <a
+          class="btn btn-primary align-self-center mb-1"
+          href="index.php?action=next&l-sequence=<?= $this->trafficLight->getLightState() ?>"
+        >
+            Suivant
+        </a>
+
+        <?php
+        if (!$isOOS): ?>
+            <a
+              class="btn btn-warning align-self-center"
+              href="index.php?action=oos"
+            >
+                Hors service
+            </a>
+        <?php
+        endif ?>
     </div>
-</body>
-</html>
+</div>
+
+<?php
+$content = ob_get_clean();
+require_once "views/layout.php";
+?>
---
+++ views/layout.php
@@ -0,0 +1,16 @@
+<?php
+/** @noinspection PhpUndefinedVariableInspection */
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Traffic Light</title>
+    <link href="/views/styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <?= $content ?>
+</body>
+</html>
